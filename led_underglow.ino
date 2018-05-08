ab3ced80850ee1d193cb46ff0dd708a04cd4394f
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-05-09 01:01:11 +0900

    cosme

--- led_underglow.ino
+++ led_underglow.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
-#define MAX_VAL 30  
-#define DELAY_TIME 50
+#define MAX_VAL 40  
+#define DELAY_TIME 100
  
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 6, NEO_GRB + NEO_KHZ800);
  
@@ -12,13 +12,11 @@ void setup() {
 void loop() {
   rainbowCycle(DELAY_TIME);
 }
- 
- 
-// Slightly different, this makes the rainbow equally distributed throughout
+  
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
  
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+  for(j=0; j<256*5; j++) { 
     for(i=0; i< strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
@@ -26,9 +24,7 @@ void rainbowCycle(uint8_t wait) {
     delay(wait);
   }
 }
- 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
+
 uint32_t Wheel(byte WheelPos) {
   if(WheelPos < 85) {
    return strip.Color((WheelPos * 3)*MAX_VAL/255, (255 - WheelPos * 3)*MAX_VAL/255, 0);
